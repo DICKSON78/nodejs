a28d12bd9f740f61f6ff074faa7ca596d238df21
DICKSON78 <augustinodickson78@gmail.com>
2025-04-27 16:38:38 +0300

    add database ssl

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: { ca: process.env.DB_SSL_CA }
 });
 
 app.post('/api/login', async (req, res) => {
